21f840a0a6145832d50582df74f44a635d58ffec
jwuestef <jwuestef@gmail.com>
2018-09-07 17:04:08 -0400

    Stopping on Friday afternoon

--- HelloMVC/Controllers/HomeController.cs
+++ HelloMVC/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using HelloMVC.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Caching;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,10 +10,30 @@ namespace HelloMVC.Controllers
 {
     public class HomeController : Controller
     {
+        ObjectCache cache = MemoryCache.Default;
+        List<Customer> customers;
+
+        public HomeController()
+        {
+            customers = cache["customers"] as List<Customer>;
+            if (customers == null)
+            {
+                customers = new List<Customer>();
+            }
+        }
+
+        public void SaveCache()
+        {
+            cache["customers"] = customers;
+        }
+
+
         public ActionResult Index()
         {
             return View();
         }
+
+
 
         public ActionResult About()
         {
@@ -20,11 +42,112 @@ namespace HelloMVC.Controllers
             return View();
         }
 
+
+
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
 
             return View();
         }
+
+
+
+        public ActionResult ViewCustomer(string id)
+        {
+            Customer customer = customers.FirstOrDefault(c => c.Id == id);
+
+            if (customer == null)
+                return HttpNotFound();
+            else
+                return View(customer);
+        }
+
+
+
+        public ActionResult AddCustomer()
+        {
+            return View();
+        }
+
+
+
+        [HttpPost]
+        public ActionResult AddCustomer(Customer customer)
+        {
+            customer.Id = Guid.NewGuid().ToString();
+            customers.Add(customer);
+            SaveCache();
+
+            return RedirectToAction("CustomerList");
+        }
+
+
+
+        public ActionResult EditCustomer(string id)
+        {
+            Customer customer = customers.FirstOrDefault(c => c.Id == id);
+
+            if (customer == null)
+                return HttpNotFound();
+            else
+                return View(customer);
+        }
+
+
+
+        [HttpPost]
+        public ActionResult EditCustomer(Customer customer, string id)
+        {
+            var customerToEdit = customers.FirstOrDefault(c => c.Id == id);
+            if (customer == null)
+                return HttpNotFound();
+            else
+            {
+                customerToEdit.Name = customer.Name;
+                customerToEdit.Telephone = customer.Telephone;
+                SaveCache();
+                return RedirectToAction("CustomerList");
+            }
+        }
+
+
+
+        public ActionResult DeleteCustomer(string id)
+        {
+            Customer customer = customers.FirstOrDefault(c => c.Id == id);
+
+            if (customer == null)
+                return HttpNotFound();
+            else
+                return View(customer);
+        }
+
+
+
+        [HttpPost]
+        [ActionName("DeleteCustomer")]
+        public ActionResult ConfirmDeleteCustomer(string id)
+        {
+            Customer customer = customers.FirstOrDefault(c => c.Id == id);
+
+            if (customer == null)
+                return HttpNotFound();
+            else
+            {
+                customers.Remove(customer);
+                return RedirectToAction("CustomerList");
+            }
+        }
+
+
+
+        public ActionResult CustomerList()
+        {
+            return View(customers);
+        }
+
+
+
     }
 }
